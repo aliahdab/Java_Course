39e1b6abf6cf548db00e6a4daa8a9b41fe3913f7
Ali Ahdab <aliahdab@gmail.com>
2020-04-05 17:21:42 +0200

    Adding java course laps

--- Labs/edaa01-workspace/ovn-lambdas/src/PersonCondition.java
+++
@@ -1,7 +0,0 @@
-
-/** Beskriver ett villkor som Person-objekt kan uppfylla. */
-public interface PersonCondition {
-	
-	/** Returnerar true om villkoret är uppfyllt, annars false. */
-	boolean test(Person p);
-}
--- Labs/edaa20-workspace/Tests/src/Newcource/ATM.java
+++
@@ -1,5 +0,0 @@
-package Newcource;
-
-public interface ATM {
-
-}
